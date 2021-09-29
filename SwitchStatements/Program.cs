f5a9eeea544119ea4f51760eee3afaab22de0087
nehamelville <neha.melville@gmail.com>
2021-09-29 07:56:11 -0700

    initial commit

---
+++ SwitchStatements/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace SwitchStatements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("What is your favorite school subject ? ");
+            var favSubject =( Console.ReadLine()).ToLower();
+
+             switch(favSubject)
+            {
+                case "math":
+
+                        Console.WriteLine($" {favSubject} is my favorite subject!!");
+                        break;
+
+                case "english":
+
+                        Console.WriteLine($" {favSubject} is  my favorite subject!!");
+                        break;
+                
+                case "science":
+
+                       Console.WriteLine($" {favSubject} is  my favorite subject!!");
+                       break;
+                    
+                case "social science":
+
+                        Console.WriteLine($" {favSubject} is  my favorite subject!!");
+                        break;
+                    
+                case "politics":
+
+                        Console.WriteLine($" {favSubject} is  my favorite subject!!");
+                        break;
+
+                default :
+
+                        Console.WriteLine($"case not handled!!");
+                        break;
+        
+
+            }
+
+        }
+    }
+}
